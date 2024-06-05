805e315959d42840b2a07a317a79639f48ca0ae2
Larissa Raimee Gomes <srta.raimee@gmail.com>
2024-06-05 15:00:49 -0300

    TUDO COMENTADO PELA LARI

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ import ast
 
 dados = pd.read_csv("tabela.csv")
 
+<<<<<<< Updated upstream
 paito = paito.Grafo(repr="lista", direcionado=False, ponderado=False)
+=======
+paito = paito.Grafo(repr="matriz", direcionado=True, ponderado=True)
 
+>>>>>>> Stashed changes
+
+paito.adicionarVertice("S")
 paito.adicionarVertice("A")
 paito.adicionarVertice("B")
 paito.adicionarVertice("C")
 paito.adicionarVertice("D")
 paito.adicionarVertice("E")
-paito.adicionarVertice("F")
+
 paito.adicionarVertice("G")
+<<<<<<< Updated upstream
 # paito.adicionarVertice("H")
 
 
@@ -29,3 +36,27 @@ paito.adicionarAresta('E', 'F')
 
 
 print(paito.averageGeodesicDistance())
+=======
+paito.adicionarVertice("H")
+paito.adicionarVertice("I")
+paito.adicionarVertice("J")
+paito.adicionarVertice("K")
+
+
+paito.adicionarAresta("S", "A")
+paito.adicionarAresta("A", "B")
+paito.adicionarAresta("A", "C")
+
+paito.adicionarAresta("S", "H")
+
+
+paito.adicionarAresta("B", "E")
+paito.adicionarAresta("B", "D")
+
+paito.adicionarAresta("C", "G")
+paito.adicionarAresta("H", "I")
+paito.adicionarAresta("H", "J")
+paito.adicionarAresta("I", "K")
+
+print(paito.buscaProfundidadeComFinal("S", "G"))
+>>>>>>> Stashed changes
